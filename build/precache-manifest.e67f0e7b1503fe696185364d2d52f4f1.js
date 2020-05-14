86fb8f8d31191705a8ae3eb9edc10f43ffb56d9d
chakal05 <chakirrasta@gmail.com>
2020-05-14 22:53:53 +0200

    testing deploy

---
+++ build/precache-manifest.e67f0e7b1503fe696185364d2d52f4f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d2251604ea349d5848d3841253f279",
+    "url": "/index.html"
+  },
+  {
+    "revision": "043edf1f71277920ec61",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0d12d5b4eaedaa9b2588",
+    "url": "/static/css/main.da832dbd.chunk.css"
+  },
+  {
+    "revision": "043edf1f71277920ec61",
+    "url": "/static/js/2.ca44719e.chunk.js"
+  },
+  {
+    "revision": "e294ecca09c988948a71490fa3e317bd",
+    "url": "/static/js/2.ca44719e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d12d5b4eaedaa9b2588",
+    "url": "/static/js/main.5579beac.chunk.js"
+  },
+  {
+    "revision": "196cc40af19de3fc4c2a",
+    "url": "/static/js/runtime-main.81903876.js"
+  }
+]);
